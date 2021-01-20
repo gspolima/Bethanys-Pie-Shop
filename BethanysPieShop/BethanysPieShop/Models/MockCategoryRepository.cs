f62283f4eed9fa914fc91726211af0a1ceb4e148
Gustavo Sampaio <sampaioglima@gmail.com>
2021-01-19 23:16:27 -0300

    Added mock repositories for both Pie and Category,  hard coding some data inside.

---
+++ BethanysPieShop/BethanysPieShop/Models/MockCategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories
+        { get => new List<Category>()
+            {
+                new Category() { CategoryId = 1, Name = "Cheese Cakes", Description = "Cheesy all the way" },
+                new Category() { CategoryId = 2, Name = "Fruit Pies", Description = "All-fruity pies" },
+                new Category() { CategoryId = 3, Name = "Seasonal Pies", Description = "Get in the mood for a seasonal pie" }
+            };
+        }
+    }
+}
---
+++ BethanysPieShop/BethanysPieShop/Models/MockPieRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class MockPieRepository : IPieRepository
+    {
+        private readonly ICategoryRepository categoryRepository = new MockCategoryRepository();
+
+        public IEnumerable<Pie> AllPies => new List<Pie>()
+        {
+            new Pie() { PieId = 1, Name = "Strawberry Pie", Price = (double)15.47, ShortDescription = "Made with fresh strawberries"},
+            new Pie() { PieId = 2, Name = "Rhubarb Pie", Price = (double)5.50, ShortDescription = "Delicious organic rhubarb"},
+            new Pie() { PieId = 3, Name = "Pumpkin Pie", Price = (double)12.00, ShortDescription = "Sweet pieces of pumpkin"},
+            new Pie() { PieId = 4, Name = "Cheese Cake", Price = (double)20.00, ShortDescription = "The latest and the greatest"}
+        };
+
+        public IEnumerable<Pie> PiesOfTheWeek { get; }
+
+        public Pie GetPieById(int pieId)
+        {
+            return AllPies.FirstOrDefault(p => p.PieId == pieId);
+        }
+
+        public IEnumerable<Pie> GetPiesInStock()
+        {
+            return AllPies.Where(p => p.InStock == true);
+        }
+    }
+}
